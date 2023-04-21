f3b78e33cedb001b8aa5bcd6014c9721a43fc628
Rouni Gorgees <rounigorgees@gmail.com>
2023-04-21 13:32:03 -0400

    add display for the information that the user enters to the input and ask the user to  confirm that if he/she has entered the required information right or not

--- save_password.py
+++ save_password.py
@@ -1,18 +1,24 @@
 import json
 import os
 
+from display_password import display_password
+
 
 def save_password(website_name, website_username, website_password, website_url, website_email, additional_info):
     if os.path.exists('passwords.json'):
         with open('passwords.json', 'r') as f:
-            data = json.load(f)
+            try:
+                data = json.load(f)
+            except json.decoder.JSONDecodeError:
+                print("Error: passwords.json is empty or contains invalid JSON data.")
+                return
     else:
         data = {'passwords': []}
 
     passwords = data.get('passwords', [])
 
     if website_name.lower() in [x['website'].lower() for x in passwords]:
-        print("Password for this website already exists.")
+        print("The website already exists.")
         return
 
     new_password = {
@@ -23,10 +29,16 @@ def save_password(website_name, website_username, website_password, website_url,
         "email": website_email,
         "additional_info": additional_info
     }
-
-    passwords.append(new_password)
+    display_password(new_password)
+    # ask user if they want to save the password
+    savePassword = input("Do you want to save this password? (yes/no): ")
+    if savePassword.lower() == "no":
+        # if user doesn't want to save the password, give them the option to create a new password
+        print("Password not saved.")
+    elif savePassword.lower() == "yes":
+        passwords.append(new_password)
+        print("Password saved successfully.")
+        display_password(new_password)
 
     with open('passwords.json', 'w') as f:
         json.dump(data, f, indent=4)
-
-    print("Password saved successfully.")
